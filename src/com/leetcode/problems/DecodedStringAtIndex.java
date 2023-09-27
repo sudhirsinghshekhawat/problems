c4b0313b836fe5482103d5977d7aebf774c146b5
sudhir <sudhirsinghshekhawat@gmail.com>
2023-09-27 22:15:22 +0530

    Leetcode: 880  Decoding String at Index

---
+++ src/com/leetcode/problems/DecodedStringAtIndex.java
@@ -0,0 +1,46 @@
+package com.leetcode.problems;
+
+public class DecodedStringAtIndex {
+
+    public String decodeAtIndex(String s, int k) {
+        long size = 0;
+        int n = s.length();
+
+        // Calculate the size of the decoded string
+        for (int i = 0; i < n; i++) {
+            if (Character.isDigit(s.charAt(i))) {
+                size *= s.charAt(i) - '0';
+            } else {
+                size++;
+            }
+        }
+
+        // Start decoding from the end of the string
+        for (int i = n - 1; i >= 0; i--) {
+            k = (int) (k % size);
+
+            if (k == 0 && Character.isLetter(s.charAt(i))) {
+                return String.valueOf(s.charAt(i));
+            }
+
+            if (Character.isDigit(s.charAt(i))) {
+                size /= s.charAt(i) - '0';
+            } else {
+                size--;
+            }
+        }
+
+        return "";
+    }
+
+    public static void main(String[] args) {
+        String s = "leet2code3";
+        int k = 10;
+
+        DecodedStringAtIndex decodedStringAtIndex = new DecodedStringAtIndex();
+        String res = decodedStringAtIndex.decodeAtIndex(s, k);
+        System.out.println(res);
+
+    }
+
+}
