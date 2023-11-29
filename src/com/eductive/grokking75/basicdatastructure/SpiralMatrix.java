702a52aa0883576d40d1235c892fba1ed91ba6aa
sudhir <sudhirsinghshekhawat@gmail.com>
2023-11-30 00:03:41 +0530

    Spiral traversal of matrix

---
+++ src/com/eductive/grokking75/basicdatastructure/SpiralMatrix.java
@@ -0,0 +1,42 @@
+package com.eductive.grokking75.basicdatastructure;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class SpiralMatrix {
+
+    public static List<Integer> spiralOrder(int[][] matrix) {
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        int row = 0;
+        int col = -1;
+        int direction = 1;
+
+        List<Integer> result = new ArrayList<>();
+
+        while (rows > 0 && cols > 0) {
+            for (int i = 0; i < cols; i++) {
+                col += direction;
+                result.add(matrix[row][col]);
+            }
+            rows--;
+
+            for (int i = 0; i < rows; i++) {
+                row += direction;
+                result.add(matrix[row][col]);
+            }
+            cols--;
+
+            direction *= -1;
+
+        }
+
+        return result;
+
+
+    }
+
+}
